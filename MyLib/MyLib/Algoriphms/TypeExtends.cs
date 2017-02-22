d45d04f55abfc5740bdd0eac1a430f8c2db6cdca
Данил <redreev.danil@yandex.ru>
2017-02-22 14:43:19 +0500

    little helpful thing

---
+++ MyLib/MyLib/Algoriphms/TypeExtends.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLib.Algoriphms
+{
+    public static class TypeExtends
+    {
+        static readonly Type objectType = typeof(object);
+
+        public static int GetGeneration(this Type type, Type parentType)
+        {
+            int i = 0;
+            while(type != objectType)
+            {
+                if(type == parentType)
+                    return i;
+
+                ++i;
+                type = type.BaseType;
+            }
+            if (parentType == objectType)
+                return i;
+            return -1;
+        }
+    }
+}
---
+++ MyLib/MyLibTests/Algorithms/TestTypeExtends.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib.Algoriphms;
+
+namespace MyLibTests.Algorithms
+{
+    [TestClass]
+    public class TestTypeExtends
+    {
+        [TestMethod]
+        public void TestGetGeneration()
+        {
+            // int : ValueType, object
+            // int is the third generation relatively object, so it has 2 parents
+            Assert.AreEqual(2, typeof(int).GetGeneration(typeof(object)));
+
+            // parent type int is the tested type.
+            Assert.AreEqual(0, typeof(int).GetGeneration(typeof(int)));
+
+            // float is not a base type of int
+            Assert.AreEqual(-1, typeof(int).GetGeneration(typeof(float)));
+
+        }
+    }
+}
