9da57e969b7252056853e0e52f25901c83ac83d7
Данил <redreev.danil@yandex.ru>
2017-04-05 16:50:36 +0500

    new CustomTree

---
+++ MyLib/MyLib/Structures/Tree/CustomTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRLib.Structures.Tree
+{
+    public struct CustomTree<T> : IHaveChilds<CustomTree<T>>
+    {
+        public delegate IEnumerable<T> ChildsGetter(T root);
+
+        T tree;
+        ChildsGetter childsOf;
+
+        IEnumerable<CustomTree<T>> IHaveChilds<CustomTree<T>>.childs
+        {
+            get { return new ChildSet(this); }
+        }
+
+        public CustomTree(T tree, ChildsGetter childs)
+        {
+            this.tree = tree;
+            this.childsOf = childs;
+        }
+
+        struct ChildSet : IEnumerable<CustomTree<T>>
+        {
+            CustomTree<T> root;
+
+            public ChildSet(CustomTree<T> root)
+            {
+                this.root = root;
+            }
+
+            public IEnumerator<CustomTree<T>> GetEnumerator()
+            {
+                foreach (var c in root.childsOf(root.tree))
+                    yield return new CustomTree<T>(c, root.childsOf);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
